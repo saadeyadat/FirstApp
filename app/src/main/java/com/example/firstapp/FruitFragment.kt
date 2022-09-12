bb49bb74e32e7782a77b8c656aebf27f804f0e09
saad <saad.eyadat.97@hotmail.com>
2022-09-12 10:19:02 +0300

    RecycleView

--- app/src/main/java/com/example/firstapp/FruitFragment.kt
+++ app/src/main/java/com/example/firstapp/FruitFragment.kt
@@ -11,14 +11,13 @@ import androidx.fragment.app.Fragment
 
 class FruitFragment(private val fruit: Fruit): Fragment(R.layout.fruit_fragment) {
 
-    private val fruitFragmentInfo = FruitFragmentInfo()
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private val fruitFragmentInfo = FruitFragmentInfo(fruit)
+    override fun onResume() {
+        super.onResume()
         val fruitTextView = activity?.findViewById<TextView>(R.id.fruit_title)  // ? - is to insure that we don't give null
         val fruitImageView = activity?.findViewById<ImageView>(R.id.fruit_image)
         val fruitInfoView = activity?.findViewById<TextView>(R.id.fruit_info)
         val exitMainButton = activity?.findViewById<ImageButton>(R.id.exit_button)
-        val exitInfoButton = activity?.findViewById<ImageView>(R.id.exit_button_info)
 
         val name = requireArguments().getString("fruitName")
         val image = requireArguments().getInt("fruitImage")
@@ -26,15 +25,8 @@ class FruitFragment(private val fruit: Fruit): Fragment(R.layout.fruit_fragment)
         fruitTextView?.text = name.toString()
         fruitImageView?.setImageResource(image)
 
-        val fruitInfo = fruit.info
-        fruitInfoView?.text = fruitInfo.toString()
-
         fruitInfoView?.setOnClickListener {
             childFragmentManager.beginTransaction().replace(R.id.fragment_view_info, fruitFragmentInfo).commit()
-        }
-
-        exitInfoButton?.setOnClickListener {
-            childFragmentManager.beginTransaction().remove(fruitFragmentInfo).commit()
         }
 
         exitMainButton?.setOnClickListener {
--- app/src/main/java/com/example/firstapp/FruitFragmentInfo.kt
+++ app/src/main/java/com/example/firstapp/FruitFragmentInfo.kt
@@ -3,17 +3,27 @@ package com.example.firstapp
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 
-class FruitFragmentInfo: Fragment(R.layout.fruit_info_fragment) {
+class FruitFragmentInfo(private val fruit: Fruit): Fragment(R.layout.fruit_info_fragment) {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onResume() {
+        super.onResume()
         val fruitInfo = activity?.findViewById<TextView>(R.id.fragment_view_on_info)
+        val addText = activity?.findViewById<EditText>(R.id.add_text)
+        val addButton = activity?.findViewById<ImageButton>(R.id.add_button)
+        val exitButton = activity?.findViewById<ImageButton>(R.id.exit_button2)
 
-        fruitInfo?.text = "info."
+        addButton?.setOnClickListener {
+            fruitInfo?.text = addText?.text.toString()
+        }
+
+        exitButton?.setOnClickListener {
+            parentFragmentManager.beginTransaction().remove(this).commit()
+        }
     }
 }
